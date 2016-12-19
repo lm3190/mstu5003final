6c15845098fe2b2cf720d553275c65327e539164
lm3190 <lm3190@tc.columbia.edu>
2016-12-18 21:50:57 -0500

    Complete Final
    Most up to date final with all attached files

--- finalprojectlm3190/main.js
+++ finalprojectlm3190/main.js
@@ -1,22 +1,10 @@
-// $scope.verticalSlider1 = {
-//     value: 0,
-//     options: {
-//         floor: 0,
-//         ceil: 10,
-//         vertical: true
-//     }
-// };
 
 
+$("input[type='range']").addClass('slideThumb');
 
 $(document).ready(function() {
     $('[data-toggle="tooltip"]').tooltip();
 });
-
-
-
-// range = new JSlider();
-// slider.putClientProperty("Slider.paintThumbArrowShape", Boolean.TRUE);
 
 
 function playSound() {
@@ -29,4 +17,49 @@ function playSound() {
 
 $(document).ready(function() {
     $('[data-toggle="popover"]').popover();
+});
+
+var totalScore = 0;
+
+document.querySelector('#evalQuiz').addEventListener('click', function(event){
+	var q1Val = document.querySelector('[name="one"]:checked').value;
+	var q2Val = document.querySelector('[name="two"]:checked').value;
+	var q3Val = document.querySelector('[name="three"]:checked').value;
+  	var q4Val = document.querySelector('[name="four"]:checked').value;
+	var q5Val = document.querySelector('[name="five"]:checked').value;
+  	var q6Val = document.querySelector('[name="six"]:checked').value;
+
+	if (q1Val === "a") {
+		totalScore++;
+	}
+
+	if (q2Val === "b") {
+		totalScore++;
+	}
+
+  if (q3Val === "a") {
+		totalScore++;
+	}
+
+  if (q4Val === "a") {
+    totalScore++;
+  }
+
+  if (q5Val === "b") {
+    totalScore++;
+  }
+
+  if (q6Val === "a") {
+    totalScore++;
+  }
+	var feedbackEl = document.getElementById('quizFeedback');
+
+	feedbackEl.innerHTML = "Your score was: " + totalScore + "/6 questions.";
+
+	if (totalScore < 6) {
+		feedbackEl.classList.add('badQuiz');
+	} else {
+		feedbackEl.classList.add('goodQuiz');
+	}
+
 });
